9afe4d9310a6551282943c8f0e6a59a0e3f280a2
Stuti_Srivastava <25113ew027@glbitm.ac.in>
2026-06-08 19:19:18 +0530

    Add files via upload

---
+++ Day_8/Day_8/Alphabet_Pyramid.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Alphabet_Pyramid {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int i, j;
+        
+        for(i =65; i<=69; i++){
+            for(j=65; j<=i; j++){
+                System.out.print((char)j + " ");
+            }
+        
+        System.out.println();
+        }
+    }
+}
---
+++ Day_8/Day_8/Half_Pyramid.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Half_Pyramid {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int i, j, n ;
+        System.out.println("Enter a number:");
+        n = sc.nextInt();
+        for(i =1; i<=n; i++){
+            for(j=1; j<=i; j++){
+                System.out.print(" * " );
+            }
+        
+        System.out.println();
+        }
+    }
+}
