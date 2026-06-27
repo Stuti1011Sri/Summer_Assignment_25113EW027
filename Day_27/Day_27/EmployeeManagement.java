51cf552a7229214f8cacc1340eeea3d0051b41f0
Stuti_Srivastava <25113ew027@glbitm.ac.in>
2026-06-27 22:51:24 +0530

    Add files via upload

---
+++ Day_27/Day_27/EmployeeManagement.java
@@ -0,0 +1,58 @@
+
+import java.util.*;
+
+class Employee {
+    int id;
+    String name;
+    String department;
+
+    Employee(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    void display() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Department: " + department);
+    }
+}
+
+public class EmployeeManagement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        while (true) {
+            System.out.println("\n--- Employee Management ---");
+            System.out.println("1. Add Employee");
+            System.out.println("2. View Employees");
+            System.out.println("3. Exit");
+            System.out.print("Enter choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Enter Department: ");
+                    String dept = sc.nextLine();
+                    employees.add(new Employee(id, name, dept));
+                    System.out.println("Employee added successfully!");
+                    break;
+                case 2:
+                    for (Employee e : employees) {
+                        e.display();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }
+    }
+}
